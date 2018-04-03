c97dfa5c10f59bb375b44645141ebc73efb85794
davide.piona <dadepio@gmail.com>
2018-04-03 12:54:47 +0200

    Added Eureka / Configured ports

--- gateway/src/main/java/com/benchmark/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/benchmark/gateway/GatewayApplication.java
@@ -3,9 +3,7 @@ package com.benchmark.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class GatewayApplication {
 
