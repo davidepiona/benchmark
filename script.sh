2e6c6f13d9b232883b0eac950fe72c2c4de934da
davidepiona <dadepio@gmail.com>
2018-05-02 10:52:02 +0200

    added time in script, added collection of statistics

--- script.sh
+++ script.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 for i in `seq 1 10`
 do
-	wget http://localhost:8010/api/movies #| Select-Object -Property Content
+	time wget -O/dev/null -q http://localhost:8010/api/movies #| Select-Object -Property Content 
+	
+	time curl -X POST \
+	  http://localhost:8010/api/movies \
+	  -H 'Cache-Control: no-cache' \
+	  -H 'Content-Type: application/json' \
+	  -H 'Postman-Token: 350421ff-1c55-460b-ad09-2016ee02fa48' \
+	  -d '{
+	    "title": "Titanic",
+	    "director": "James Cameron",
+	    "releaseDate": "1997-11-01",
+	    "language": "English",
+	    "pending": false
+	}
+	'
 
 	echo $i
 done
+
