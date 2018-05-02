e36532b789d920dfc9dd9cdcccb2c65cdc39dee3
davidepiona <dadepio@gmail.com>
2018-05-02 14:39:45 +0200

    Fixed cassandra memory usage with docker-compose, created script for push

---
+++ docker/dockerPush.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo docker push davidepiona/upload-microservice
+sudo docker push davidepiona/registry-microservice
+sudo docker push davidepiona/eureka-microservice
+sudo docker push davidepiona/gateway-microservice
+sudo docker push davidepiona/benchgui
+
