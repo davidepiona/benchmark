d97deaf7a295bba12e37bfffef0d47f26dccbb22
davidepiona <dadepio@gmail.com>
2018-05-31 11:04:17 +0200

    Fixed script for collecting harware statistics

--- docker/hardwareStats.sh
+++ docker/hardwareStats.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-printf "Memory\t\t Disk\t\tCPU\n"
+printf "Timestamp, Memory, Disk, , , CPU\n"
 rm disk.txt
 touch disk.txt
+DATE=$(date +"%m%d_%H:%M:%S")
 iostat -dxt /dev/sda 1 > disk.txt &
 while true; do
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }') 
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }') 
 CPU=$(expr 100 - $(vmstat 1 2|tail -1|awk '{print $15}'))
 DISK=$(cat disk.txt | tail -2 | rev | cut -c 1-6 | rev)
-echo "$MEMORY$DISK\t\t$CPU"
+echo "$DATE,$MEMORY,$DISK,$CPU"
 done
 rm disk.txt
--- wsTest/startTest.sh
+++ wsTest/startTest.sh
@@ -1,4 +1,4 @@
-java -Xmx384m-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false \
+java -Xmx384m -Dspring.profiles.active=kvm -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false \
     -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=7070 -Dcom.sun.management.jmxremote.rmi.port=7070 \
-    -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=0.0.0.0 -Dcom.sun.management.jmxremote.host=0.0.0.0 \
-    -jar target/wsTest-0.0.1-SNAPSHOT.jar
+    -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=10.0.1.78 -Dcom.sun.management.jmxremote.host=10.0.1.78 \
+    -jar wsTest-0.0.1-SNAPSHOT.jar
